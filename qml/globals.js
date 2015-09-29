16fd19106c2380db5da47437607413622adc2d07
zeburon <lukas@munix.at>
2015-09-29 20:27:18 +0200

    version number updated to 1.2

--- qml/globals.js
+++ qml/globals.js
@@ -1,4 +1,4 @@
-var VERSION                                 = "1.0";
+var VERSION                                 = "1.2";
 var UPDATE_POWER_INTERVAL                   = 5;
 var UPDATE_ENERGY_INTERVAL                  = 600;
 var LARGE_GRAPH_DAY_COUNT                   = 5;
