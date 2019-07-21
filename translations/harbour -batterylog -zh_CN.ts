9b24da1f42a4047ea4a6994f799c93ea2d22913a
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-07-21 13:00:19 +0800

    Create harbour -batterylog -zh_CN.ts

---
+++ translations/harbour -batterylog -zh_CN.ts
@@ -0,0 +1,541 @@
+<?xml version="1.0" encoding="utf-8"?>
+
+<!DOCTYPE TS>
+
+<TS version="2.1" language="zh_CN">
+
+<context>
+
+    <name>AboutPage</name>
+
+    <message>
+
+        <source>Battery Log</source>
+
+        <translation>电池日志</translation>
+
+    </message>
+
+    <message>
+
+        <source>Version %1</source>
+
+        <translation>版本 %1</translation>
+
+    </message>
+
+    <message>
+
+        <source>Copyright © 2015 Lukas Fraser</source>
+
+        <translation>版权所有 © 2015 Lukas Fraser</translation>
+
+    </message>
+
+    <message>
+
+        <source>This program is open source software licensed under the terms of the GNU General Public License.</source>
+
+        <translation>该项目是GNU公共许可协议下的开源软件。</translation>
+
+    </message>
+
+    <message>
+
+        <source>You can find the source code at the</source>
+
+        <translation>你可以在此找到源代码</translation>
+
+    </message>
+
+    <message>
+
+        <source>GitHub Project Page</source>
+
+        <translation>Github项目网页</translation>
+
+    </message>
+
+    <message>
+
+        <source>Inspired by SailfishOS application Hunger Meter created by Michal Hrušecký.</source>
+
+        <translation>灵感源自Michal Hrušecký 开发的旗鱼系统软件Hunger Meter。</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>ColorChooserDialog</name>
+
+    <message>
+
+        <source>Red</source>
+
+        <translation>红色</translation>
+
+    </message>
+
+    <message>
+
+        <source>Green</source>
+
+        <translation>绿色</translation>
+
+    </message>
+
+    <message>
+
+        <source>Blue</source>
+
+        <translation>蓝色</translation>
+
+    </message>
+
+    <message>
+
+        <source>Alpha</source>
+
+        <translation>Alpha</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>Event</name>
+
+    <message>
+
+        <source>Charging</source>
+
+        <translation>正在充电</translation>
+
+    </message>
+
+    <message>
+
+        <source>Discharging</source>
+
+        <translation>正在放电</translation>
+
+    </message>
+
+    <message>
+
+        <source>Full</source>
+
+        <translation>充满</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>EventPage</name>
+
+    <message>
+
+        <source>Event Log</source>
+
+        <translation>活动日志</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>Graph</name>
+
+    <message>
+
+        <source>Malfunction Detected</source>
+
+        <translation>故障监测</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>MainCover</name>
+
+    <message>
+
+        <source>Until full</source>
+
+        <translation>直到充满l</translation>
+
+    </message>
+
+    <message>
+
+        <source>Time left</source>
+
+        <translation>剩余时间</translation>
+
+    </message>
+
+    <message>
+
+        <source>Remaining</source>
+
+        <translation>待机</translation>
+
+    </message>
+
+    <message>
+
+        <source>Average</source>
+
+        <translation>平均</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>MainPage</name>
+
+    <message>
+
+        <source>About Battery Log</source>
+
+        <translation>关于Battery Log</translation>
+
+    </message>
+
+    <message>
+
+        <source>Battery Log</source>
+
+        <translation>Battery Log</translation>
+
+    </message>
+
+    <message>
+
+        <source>until full</source>
+
+        <translation>直到充满</translation>
+
+    </message>
+
+    <message>
+
+        <source>until empty</source>
+
+        <translation>直到耗尽</translation>
+
+    </message>
+
+    <message>
+
+        <source>Current</source>
+
+        <translation>当前</translation>
+
+    </message>
+
+    <message>
+
+        <source>Voltage</source>
+
+        <translation>电压</translation>
+
+    </message>
+
+    <message>
+
+        <source>when unplugged now</source>
+
+        <translation>拔出充电插头translation>
+
+    </message>
+
+    <message>
+
+        <source>when fully charged</source>
+
+        <translation>完全充满</translation>
+
+    </message>
+
+    <message>
+
+        <source>Energy</source>
+
+        <translation>电量</translation>
+
+    </message>
+
+    <message>
+
+        <source>Health</source>
+
+        <translation>健康</translation>
+
+    </message>
+
+    <message>
+
+        <source>on</source>
+
+        <translation>进行</translation>
+
+    </message>
+
+    <message>
+
+        <source>discharging</source>
+
+        <translation>放点</translation>
+
+    </message>
+
+    <message>
+
+        <source>charging</source>
+
+        <translation>正在充电</translation>
+
+    </message>
+
+    <message>
+
+        <source>more data required</source>
+
+        <translation>更多数据请求</translation>
+
+    </message>
+
+    <message>
+
+        <source>standby</source>
+
+        <translation>待命</translation>
+
+    </message>
+
+    <message>
+
+        <source>fully charged</source>
+
+        <translation>完全充满</translation>
+
+    </message>
+
+    <message>
+
+        <source>Settings</source>
+
+        <translation>设置</translation>
+
+    </message>
+
+    <message>
+
+        <source>calculating...</source>
+
+        <translation>计算中......</translation>
+
+    </message>
+
+    <message>
+
+        <source>Displaying %1 day(s)</source>
+
+        <translation>亮屏 %1 天</translation>
+
+    </message>
+
+    <message>
+
+        <source>Starting now</source>
+
+        <translation>现在开始使用</translation>
+
+    </message>
+
+    <message>
+
+        <source>Starting %1 day(s) ago</source>
+
+        <translation>%1 天前开始使用</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>SettingsPage</name>
+
+    <message>
+
+        <source>Settings</source>
+
+        <translation>设置</translation>
+
+    </message>
+
+    <message>
+
+        <source>Storage period</source>
+
+        <translation>储存周期</translation>
+
+    </message>
+
+    <message>
+
+        <source> days</source>
+
+        <translation>天</translation>
+
+    </message>
+
+    <message>
+
+        <source>Reset Colors</source>
+
+        <translation>重置颜色</translation>
+
+    </message>
+
+    <message>
+
+        <source>%1 events of %2 days stored</source>
+
+        <translation>%1 个日志 %2 天储存</translation>
+
+    </message>
+
+    <message>
+
+        <source>Charging + on</source>
+
+        <translation>正在充电</translation>
+
+    </message>
+
+    <message>
+
+        <source>Charging + standby</source>
+
+        <translation>充电待命</translation>
+
+    </message>
+
+    <message>
+
+        <source>Discharging + on</source>
+
+        <translation>正在放电</translation>
+
+    </message>
+
+    <message>
+
+        <source>Discharging + standby</source>
+
+        <translation>放电待命</translation>
+
+    </message>
+
+    <message>
+
+        <source>Line Colors</source>
+
+        <translation>线条色调</translation>
+
+    </message>
+
+    <message>
+
+        <source>Deleting %1 days</source>
+
+        <translation>正在删除 %1 天</translation>
+
+    </message>
+
+    <message>
+
+        <source>Clearing logs</source>
+
+        <translation>清空日志中</translation>
+
+    </message>
+
+    <message>
+
+        <source>Clear logs</source>
+
+        <translation>清空日志</translation>
+
+    </message>
+
+    <message>
+
+        <source>Storage</source>
+
+        <translation>Lagring</translation>
+
+    </message>
+
+</context>
+
+<context>
+
+    <name>timeformat</name>
+
+    <message>
+
+        <source>%1 minute(s)</source>
+
+        <translation>%1 分钟</translation>
+
+    </message>
+
+    <message>
+
+        <source>%1 hour(s)</source>
+
+        <translation>%1 小时</translation>
+
+    </message>
+
+    <message>
+
+        <source> and </source>
+
+        <translation>及</translation>
+
+    </message>
+
+    <message>
+
+        <source>%1 day(s)</source>
+
+        <translation>%1 天</translation>
+
+    </message>
+
+    <message>
+
+        <source>%1:%2</source>
+
+        <translation>%1:%2</translation>
+
+    </message>
+
+</context>
+
+</TS>
