6ac76ccd01caa8355b0f5fad66e0d4f5382770a8
mankysee <mankysee@newjots.com>
2023-12-16 14:41:40 +0300

    CHANGE: Upped a version and added an option to display 15 days at a time and an option to keep logs for 10000 days

--- qml/globals.js
+++ qml/globals.js
@@ -1,12 +1,12 @@
-var VERSION                                 = "1.3.2";
+var VERSION                                 = "1.3.3";
 var UPDATE_POWER_INTERVAL                   = 5;
 var UPDATE_ENERGY_INTERVAL                  = 600;
-var LARGE_GRAPH_DAY_COUNTS                  = [1, 2, 5, 10];
+var LARGE_GRAPH_DAY_COUNTS                  = [1, 2, 5, 10, 15];
 var DEFAULT_LARGE_GRAPH_DAY_COUNT           = 5;
 var LARGE_GRAPH_DAY_COUNT_LONGTERM          = 10;
 var SMALL_GRAPH_DAY_COUNT                   = 2;
 var EVENT_PAGE_DAY_COUNT                    = 30;
-var ENERGY_LOG_DAY_COUNTS                   = [5, 10, 15, 20, 30, 50, 100];
+var ENERGY_LOG_DAY_COUNTS                   = [5, 10, 15, 20, 30, 50, 100, 10000];
 var DEFAULT_ENERGY_LOG_DAY_COUNT            = 30;
 var DEFAULT_LINE_COLOR_CHARGING_ACTIVE      = "#ffc0cc00";
 var DEFAULT_LINE_COLOR_CHARGING_INACTIVE    = "#cc66a621";
